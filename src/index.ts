f6a2d4f7d37e0deb599d7b4f137455b7ded6925d
daishi <daishi@axlight.com>
2024-04-18 22:05:33 +0900

    detect name collisions

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,95 @@
-/* eslint no-restricted-syntax: off */
+type ParametersIf<T> = T extends (...args: infer Args) => any ? Args : never;
 
 type SliceConfig<
   Name extends string,
-  SliceValue,
+  Value,
   Actions extends {
-    [actionName: string]: (
-      ...args: never[]
-    ) => (slice: SliceValue) => SliceValue;
+    [actionName: string]: (...args: never[]) => (slice: Value) => Value;
   },
 > = {
   name: Name;
-  value: SliceValue;
+  value: Value;
   actions: Actions;
 };
 
 type InferState<Configs> = Configs extends [
-  SliceConfig<infer Name, infer SliceValue, infer Actions>,
+  SliceConfig<infer Name, infer Value, infer Actions>,
   ...infer Rest,
 ]
-  ? { [name in Name]: SliceValue } & {
+  ? { [name in Name]: Value } & {
       [actionName in keyof Actions]: (
         ...args: Parameters<Actions[actionName]>
       ) => void;
     } & InferState<Rest>
   : unknown;
 
+type IsDupicated<Name, Names extends unknown[]> = Names extends [
+  infer One,
+  ...infer Rest,
+]
+  ? One extends Name
+    ? true
+    : IsDupicated<Name, Rest>
+  : false;
+
+type HasDuplicatedNames<
+  Configs,
+  Names extends string[] = [],
+> = Configs extends [
+  SliceConfig<infer Name, infer _Value, infer Actions>,
+  ...infer Rest,
+]
+  ? Name extends Names[number]
+    ? true
+    : IsDupicated<keyof Actions, Names> extends true
+      ? true
+      : HasDuplicatedNames<Rest, [Name, ...Names]>
+  : false;
+
+type HasDuplicatedArgs<Configs, State> = Configs extends [
+  SliceConfig<infer _Name, infer _Value, infer Actions>,
+  ...infer Rest,
+]
+  ? {
+      [actionName in keyof State]: ParametersIf<State[actionName]>;
+    } extends {
+      [actionName in keyof Actions]: Parameters<Actions[actionName]>;
+    }
+    ? HasDuplicatedArgs<Rest, State>
+    : true
+  : false;
+
+type IsValidConfigs<Configs> =
+  HasDuplicatedNames<Configs> extends true
+    ? false
+    : HasDuplicatedArgs<Configs, InferState<Configs>> extends true
+      ? false
+      : true;
+
 export function createSlice<
   Name extends string,
-  SliceValue,
+  Value,
   Actions extends {
-    [actionName: string]: (
-      ...args: never[]
-    ) => (slice: SliceValue) => SliceValue;
+    [actionName: string]: (...args: never[]) => (slice: Value) => Value;
   },
->(config: SliceConfig<Name, SliceValue, Actions>) {
+>(config: SliceConfig<Name, Value, Actions>) {
   return config;
 }
 
 export function withSlices<Configs extends SliceConfig<string, unknown, any>[]>(
   ...configs: Configs
-): (
-  set: (
-    fn: (prevState: InferState<Configs>) => Partial<InferState<Configs>>,
-  ) => void,
-) => InferState<Configs> {
-  return (set) => {
+): IsValidConfigs<Configs> extends true
+  ? (
+      set: (
+        fn: (prevState: InferState<Configs>) => Partial<InferState<Configs>>,
+      ) => void,
+    ) => InferState<Configs>
+  : never {
+  return ((
+    set: (
+      fn: (prevState: InferState<Configs>) => Partial<InferState<Configs>>,
+    ) => void,
+  ) => {
     const state: any = {};
     const actionNameSet = new Set<string>();
     for (const config of configs) {
@@ -69,5 +114,5 @@ export function withSlices<Configs extends SliceConfig<string, unknown, any>[]>(
       };
     }
     return state;
-  };
+  }) as never;
 }
