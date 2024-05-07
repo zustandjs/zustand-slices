babb8435985421de07943a7a7b113fb19dfd9cd8
Grigoris Christainas <grigorisxristainas@gmail.com>
2024-05-07 11:28:41 +0300

    Implement `IsValidActions` type (#14)
    * feat: implement IsValidActions type
    
    * refactor: group type tests using describe
    
    * test: add type tests for withActions

--- tests/02_type.spec.tsx
+++ tests/02_type.spec.tsx
@@ -1,181 +1,344 @@
-import { test } from 'vitest';
+import { test, describe } from 'vitest';
 import { expectType } from 'ts-expect';
 import type { TypeEqual } from 'ts-expect';
 
-import { createSlice, withSlices } from 'zustand-slices';
+import { createSlice, withSlices, withActions } from 'zustand-slices';
 
-test('slice type: single slice', () => {
-  const countSlice = createSlice({
-    name: 'count',
-    value: 0,
-    actions: {
-      inc: () => (prev) => prev + 1,
-    },
-  });
-  expectType<
-    TypeEqual<
-      {
-        name: 'count';
-        value: number;
-        actions: {
-          inc: () => (prev: number) => number;
-        };
+describe('createSlice', () => {
+  test('slice type: single slice', () => {
+    const countSlice = createSlice({
+      name: 'count',
+      value: 0,
+      actions: {
+        inc: () => (prev) => prev + 1,
       },
-      typeof countSlice
-    >
-  >(true);
+    });
+    expectType<
+      TypeEqual<
+        {
+          name: 'count';
+          value: number;
+          actions: {
+            inc: () => (prev: number) => number;
+          };
+        },
+        typeof countSlice
+      >
+    >(true);
+  });
 });
 
-test('slice type: withSlices', () => {
-  const countSlice = createSlice({
-    name: 'count',
-    value: 0,
-    actions: {
-      inc: () => (prev) => prev + 1,
-      reset: () => () => 0,
-    },
+describe('withSlices', () => {
+  test('slice type, no collisions', () => {
+    const countSlice = createSlice({
+      name: 'count',
+      value: 0,
+      actions: {
+        inc: () => (prev) => prev + 1,
+        reset: () => () => 0,
+      },
+    });
+
+    const textSlice = createSlice({
+      name: 'text',
+      value: 'Hello',
+      actions: {
+        updateText: (newText: string) => () => newText,
+        reset: () => () => 'Hello',
+      },
+    });
+
+    type CountTextState = {
+      count: number;
+      inc: () => void;
+      text: string;
+      updateText: (newText: string) => void;
+      reset: () => void;
+    };
+
+    const slices = withSlices(countSlice, textSlice);
+
+    expectType<
+      (
+        set: (
+          fn: (prevState: CountTextState) => Partial<CountTextState>,
+        ) => void,
+      ) => CountTextState
+    >(slices);
+
+    expectType<TypeEqual<typeof slices, never>>(false);
   });
 
-  const textSlice = createSlice({
-    name: 'text',
-    value: 'Hello',
-    actions: {
-      updateText: (newText: string) => () => newText,
-      reset: () => () => 'Hello',
-    },
+  test('name collisions: same slice names', () => {
+    const countSlice = createSlice({
+      name: 'count',
+      value: 0,
+      actions: {
+        inc: () => (prev) => prev + 1,
+      },
+    });
+    const anotherCountSlice = createSlice({
+      name: 'count',
+      value: 0,
+      actions: {
+        inc: () => (prev) => prev + 1,
+      },
+    });
+    expectType<never>(withSlices(countSlice, anotherCountSlice));
   });
 
-  type CountTextState = {
-    count: number;
-    inc: () => void;
-    text: string;
-    updateText: (newText: string) => void;
-    reset: () => void;
-  };
+  test('name collisions: slice name and action name', () => {
+    const countSlice = createSlice({
+      name: 'count',
+      value: 0,
+      actions: {
+        inc: () => (prev) => prev + 1,
+      },
+    });
+    const anotherCountSlice = createSlice({
+      name: 'anotherCount',
+      value: 0,
+      actions: {
+        count: () => (prev) => prev + 1,
+      },
+    });
+    expectType<never>(withSlices(countSlice, anotherCountSlice));
+  });
 
-  const slices = withSlices(countSlice, textSlice);
+  test('name collisions: slice name and action name (overlapping case)', () => {
+    const countSlice = createSlice({
+      name: 'count',
+      value: 0,
+      actions: {
+        inc: () => (prev) => prev + 1,
+      },
+    });
+    const anotherCountSlice = createSlice({
+      name: 'anotherCount',
+      value: 0,
+      actions: {
+        count: () => (prev) => prev + 1,
+        dec: () => (prev) => prev - 1,
+      },
+    });
+    expectType<never>(withSlices(countSlice, anotherCountSlice));
+  });
 
-  expectType<
-    (
-      set: (fn: (prevState: CountTextState) => Partial<CountTextState>) => void,
-    ) => CountTextState
-  >(slices);
+  test('args collisions: different args', () => {
+    const countSlice = createSlice({
+      name: 'count',
+      value: 0,
+      actions: {
+        inc: (s: string) => (prev) => prev + (s ? 2 : 1),
+      },
+    });
+    const anotherCountSlice = createSlice({
+      name: 'anotherCount',
+      value: 0,
+      actions: {
+        inc: (n: number) => (prev) => prev + n,
+      },
+    });
+    expectType<never>(withSlices(countSlice, anotherCountSlice));
+    expectType<never>(withSlices(anotherCountSlice, countSlice));
+  });
 
-  expectType<TypeEqual<typeof slices, never>>(false);
+  test('args collisions: same args', () => {
+    const countSlice = createSlice({
+      name: 'count',
+      value: 0,
+      actions: {
+        inc: () => (prev) => prev + 1,
+      },
+    });
+    const anotherCountSlice = createSlice({
+      name: 'anotherCount',
+      value: 0,
+      actions: {
+        anotherInc: (n: number) => (prev) => prev + n,
+      },
+    });
+    expectType<(...args: never[]) => unknown>(
+      withSlices(countSlice, anotherCountSlice),
+    );
+    expectType<(...args: never[]) => unknown>(
+      withSlices(anotherCountSlice, countSlice),
+    );
+  });
+
+  test('args collisions: overload case', () => {
+    const countSlice = createSlice({
+      name: 'count',
+      value: 0,
+      actions: {
+        inc: () => (prev) => prev + 1,
+      },
+    });
+    const anotherCountSlice = createSlice({
+      name: 'anotherCount',
+      value: 0,
+      actions: {
+        inc: (n: number) => (prev) => prev + n,
+      },
+    });
+    expectType<never>(withSlices(countSlice, anotherCountSlice));
+    expectType<never>(withSlices(anotherCountSlice, countSlice));
+  });
 });
 
-test('name collisions: same slice names', () => {
-  const countSlice = createSlice({
-    name: 'count',
-    value: 0,
-    actions: {
-      inc: () => (prev) => prev + 1,
-    },
-  });
-  const anotherCountSlice = createSlice({
-    name: 'count',
-    value: 0,
-    actions: {
-      inc: () => (prev) => prev + 1,
-    },
-  });
-  expectType<never>(withSlices(countSlice, anotherCountSlice));
-});
+describe('withActions', () => {
+  test('slice type, no collisions', () => {
+    const countSlice = createSlice({
+      name: 'count',
+      value: 0,
+      actions: {
+        inc: () => (state) => state + 1,
+        set: (newCount: number) => () => newCount,
+      },
+    });
 
-test('name collisions: slice name and action name', () => {
-  const countSlice = createSlice({
-    name: 'count',
-    value: 0,
-    actions: {
-      inc: () => (prev) => prev + 1,
-    },
-  });
-  const anotherCountSlice = createSlice({
-    name: 'anotherCount',
-    value: 0,
-    actions: {
-      count: () => (prev) => prev + 1,
-    },
-  });
-  expectType<never>(withSlices(countSlice, anotherCountSlice));
-});
+    const textSlice = createSlice({
+      name: 'text',
+      value: 'Hello',
+      actions: {
+        updateText: (text: string) => () => text,
+      },
+    });
 
-test('name collisions: slice name and action name (overlapping case)', () => {
-  const countSlice = createSlice({
-    name: 'count',
-    value: 0,
-    actions: {
-      inc: () => (prev) => prev + 1,
-    },
-  });
-  const anotherCountSlice = createSlice({
-    name: 'anotherCount',
-    value: 0,
-    actions: {
-      count: () => (prev) => prev + 1,
-      dec: () => (prev) => prev - 1,
-    },
-  });
-  expectType<never>(withSlices(countSlice, anotherCountSlice));
-});
+    type CountTextState = {
+      count: number;
+      inc: () => void;
+      set: (newCount: number) => void;
+      text: string;
+      updateText: (newText: string) => void;
+      anotherInc: () => void;
+    };
 
-test('args collisions: different args', () => {
-  const countSlice = createSlice({
-    name: 'count',
-    value: 0,
-    actions: {
-      inc: (s: string) => (prev) => prev + (s ? 2 : 1),
-    },
+    expectType<
+      (
+        set: (
+          fn: (prevState: CountTextState) => Partial<CountTextState>,
+        ) => void,
+        get: () => CountTextState,
+      ) => CountTextState
+    >(
+      withActions(withSlices(countSlice, textSlice), {
+        anotherInc: () => (state) => {
+          state.set(state.count + 1);
+        },
+      }),
+    );
   });
-  const anotherCountSlice = createSlice({
-    name: 'anotherCount',
-    value: 0,
-    actions: {
-      inc: (n: number) => (prev) => prev + n,
-    },
-  });
-  expectType<never>(withSlices(countSlice, anotherCountSlice));
-  expectType<never>(withSlices(anotherCountSlice, countSlice));
-});
 
-test('args collisions: same args', () => {
-  const countSlice = createSlice({
-    name: 'count',
-    value: 0,
-    actions: {
-      inc: () => (prev) => prev + 1,
-    },
-  });
-  const anotherCountSlice = createSlice({
-    name: 'anotherCount',
-    value: 0,
-    actions: {
-      anotherInc: (n: number) => (prev) => prev + n,
-    },
-  });
-  expectType<(...args: never[]) => unknown>(
-    withSlices(countSlice, anotherCountSlice),
-  );
-  expectType<(...args: never[]) => unknown>(
-    withSlices(anotherCountSlice, countSlice),
-  );
-});
+  test('name collisions: slice names', () => {
+    const countSlice = createSlice({
+      name: 'count',
+      value: 0,
+      actions: {
+        inc: () => (state) => state + 1,
+        set: (newCount: number) => () => newCount,
+      },
+    });
 
-test('args collisions: overload case', () => {
-  const countSlice = createSlice({
-    name: 'count',
-    value: 0,
-    actions: {
-      inc: () => (prev) => prev + 1,
-    },
+    const textSlice = createSlice({
+      name: 'text',
+      value: 'Hello',
+      actions: {
+        updateText: (text: string) => () => text,
+      },
+    });
+
+    expectType<never>(
+      withActions(withSlices(countSlice, textSlice), {
+        count: () => (state) => {
+          state.set(state.text.length);
+        },
+      }),
+    );
+
+    expectType<never>(
+      withActions(withSlices(countSlice, textSlice), {
+        count: () => (state) => {
+          state.set(state.text.length);
+        },
+        text: () => (state) => {
+          state.updateText('Hello world');
+        },
+      }),
+    );
   });
-  const anotherCountSlice = createSlice({
-    name: 'anotherCount',
-    value: 0,
-    actions: {
-      inc: (n: number) => (prev) => prev + n,
-    },
+
+  test('name collisions: action names', () => {
+    const countSlice = createSlice({
+      name: 'count',
+      value: 0,
+      actions: {
+        inc: () => (state) => state + 1,
+        set: (newCount: number) => () => newCount,
+      },
+    });
+
+    const textSlice = createSlice({
+      name: 'text',
+      value: 'Hello',
+      actions: {
+        updateText: (text: string) => () => text,
+      },
+    });
+
+    expectType<never>(
+      withActions(withSlices(countSlice, textSlice), {
+        inc: () => (state) => {
+          state.set(state.count + 1);
+        },
+      }),
+    );
+
+    expectType<never>(
+      withActions(withSlices(countSlice, textSlice), {
+        inc: () => (state) => {
+          state.set(state.count + 1);
+        },
+        updateText: () => (state) => {
+          state.updateText('Hello World');
+        },
+      }),
+    );
   });
-  expectType<never>(withSlices(countSlice, anotherCountSlice));
-  expectType<never>(withSlices(anotherCountSlice, countSlice));
+
+  test('name collisions: action and slice names', () => {
+    const countSlice = createSlice({
+      name: 'count',
+      value: 0,
+      actions: {
+        inc: () => (state) => state + 1,
+        set: (newCount: number) => () => newCount,
+      },
+    });
+
+    const textSlice = createSlice({
+      name: 'text',
+      value: 'Hello',
+      actions: {
+        updateText: (text: string) => () => text,
+      },
+    });
+
+    expectType<never>(
+      withActions(withSlices(countSlice, textSlice), {
+        inc: () => (state) => {
+          state.set(state.count + 1);
+        },
+        updateText: () => (state) => {
+          state.updateText('Hello World');
+        },
+        count: () => (state) => {
+          state.set(state.text.length);
+        },
+        text: () => (state) => {
+          state.updateText('Hello world');
+        },
+      }),
+    );
+  });
 });
